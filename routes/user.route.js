f6e677c5e075100bccea90920702da83068367e4
Joominseok <jumseok12@gmail.com>
2023-02-15 19:53:29 +0900

    dosc : users => user로 수정

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const auth = require("../middleware/auth")
 
-const UserController = require("../controllers/users.controller")
+const UserController = require("../controllers/user.controller")
 const userController = new UserController()
 
 router.post("/signup", userController.signup)
